385ae14a93e86774e95aa294ee5e449eeaca8bf1
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-01-03 11:08:24 +0530

    alumini_section

---
+++ alumini.php
@@ -0,0 +1,47 @@
+<?php require_once "./libs/load.php" ?>
+
+
+<?php load_template("navbar"); ?>
+
+
+<h2 class="alumini_title">Alumini</h2>
+<h4 class="subalumini_title">Alumini</h4>
+
+<div>
+<div class="col-12">
+      <h4 class = "alumini_about">ALUMINI</h4>
+      <p class = "alumini_content">
+      Alumni association of University College of Engineering Ramanathapuram welcomes you all to join with us. This association is to foster a sense of connectedness back to the institution. Also bring in a good will and voluntary commitment to support future engineers of our institution and provide industry institute relationship and enthusiastically participate in various activities organised by the college
+      </p>
+    </div>
+  </div>
+
+
+<div class="col-12">
+      <h4 class = "alumini_about">MISSION</h4>
+      <ul>
+        <li>To act as a forum for exchange of information among its members and to act as fine link between the old students and college.</li>
+        <li>To provide students guidance for higher studies and career development as and when required by them, with help from alumni.</li>
+        <li>To maintain a compendium of all alumni in different parts of the world.</li>
+        <li>To arrange teaching and training classes to students.</li>
+      </ul>
+    </div>
+
+
+<div class="col-12">
+      <h4 class = "alumini_about">VISION</h4>
+      <ul>
+        <li>To act as a bridge among the students, staff and alumni of UCER for mutual benefit and synergy.</li>
+        <li>To build a better interaction through application of strengths and resources of alumni, student and staff to improve the quality of society.</li>
+        <li>To actively work for relationship among the alumni for their career growth.</li>
+      </ul>
+    </div>
+
+
+<div class="col-12">
+      <h4 class = "alumini_about">Alumini Registration form</h4>
+      <a class = "alumini_about" href="https://forms.gle/ojphecTW9r5WkBky9">https://forms.gle/ojphecTW9r5WkBky9</a>
+</div>
+
+
+<?php load_template("footer"); ?>
