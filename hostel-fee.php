61be0ca52bb60fb5d8017b1c68a74eb03322342e
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-01-06 10:21:13 +0530

    hostel-fee-section

--- hostel-fee.php
+++ hostel-fee.php
@@ -19,29 +19,15 @@
 
 
         <div class = "col-md-9">
-            <h2>Rules & Regulations</h2>
-            <ol>
-                <li>Students are informed to keep their belongings like money, valuable ornaments, lap tops, mobile phones, watches, ATM cards, etc. under their own & safe custody, and lock their rooms when they move out. It is to inform that hostel authorities are not responsible for the loss of any item.</li>
-                <li>Whenever students leave their rooms to attend classes or to go outing or to visit hometown or to dine or to take bath or to play etc. they have to lock their rooms, switch off electrical fittings, and close doors & windows.</li>
-                <li>Students should not waste food, electricity & water, and wastage of these resources is an unsocial deed.</li>
-                <li>Celebration of birthday party or any other function in the hostel premises in any form is strictly prohibited.</li>
-                <li>Students are admitted to hostel at the beginning of every academic year on yearly basis, and they have to vacate their rooms during summer vacation to carry out maintenance work.</li>
-                <li>To get hostel admission in the subsequent year, students have to submit hostel readmission form along with recommendations of hostel authorities.</li>
-                <li>All students admitted in hostel should pay hostel fee, monthly mess fee (on or before 20th of every month), and any other fee on or before the dead line.</li>
-                <li>Students should stay only in the allotted rooms. They are not permitted to change the allotted rooms without prior written permission from the deputy warden.</li>
-                <li>Students should not allow day-scholars, guests, strangers, etc. in their rooms.</li>
-                <li>Students should maintain strict silence in the hostel rooms, dining hall, restrooms, corridor, common area, etc. They should not cause inconvenience or disturbance to other inmates of the hostel, and should maintain discipline and decorum.</li>
-                <li>Misbehaviour like whistling, shouting, howling, etc. in the hostel premises is prohibited. Also, misbehaviour with hostel staff, mess workers, sentinel, sweepers, etc. should be avoided</li>
-                <li>Ragging is a criminal offence, and is absolutely banned. Students are informed not to involve/participate in ragging in any form, and those who involve/participate in ragging will have to face the consequences as per UGC/AICTE/University regulation or notification.</li>
-                <li>During college working hours, students are not allowed to stay in their rooms. In the case of sickness, they have to get prior written permission from the hostel staff for staying in the hostel.</li>
-                <li>Every student of the hostel should maintain their rooms in a neat and tidy manner. Possessing or consuming intoxicating items or items which are injurious to health are strictly prohibited. Hostel authorities are empowered to check any room at any time, if there is a need.</li>
-                <li>Hostel students are not allowed to keep any power driven vehicles in the hostel.</li>
-                <li>Students are not allowed to play outdoor games like football, volleyball, cricket, etc. inside the hostel building which may cause breakage/damage to the hostel properties. Also, students are informed to make use of furniture, electrical fittings & other hostel properties carefully, and should not cause any damage to the hostel properties. The cost of repair/replacement will be recovered from the student(s).</li>
-                <li>All movements from- and to- the hostel should be recorded and signed in the movement register available with the security guard. Wrong/improper entries, signing on behalf of other, tampering/damaging the register, etc. are prohibited.</li>
-                <li>The outing time for students is from 3.00PM to 6.00PM on every Saturday, and informed to strictly follow the outing time. They have to reach the hostel sharply on time.
-Hostel students should follow mess timings strictly: Breakfast: 7.30AM to 8.30AM, Lunch: 1.00PM to 1.40PM & Dinner: 7.00PM to 8.00PM, and they should not stay back in the mess hall beyond the mess time.</li>
-                <li>The hostel study hour is from 8.00PM to 10.00PM, and all hostel students are asked to use it effectively. Mobile phones can be switched off to concentrate on studies.</li>
-            </ol>
+            <h2>UCER Hostel Fees Payment</h2>
+            <div class="container-fee">
+            <img class="profile-img" src="https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png" alt="Profile Picture">
+            <div class="info">
+            <h3><a href="#">Click Here to pay Hostel Fees</a></h3>
+                <p>Online Payment Only Accepted.</p>
+                </div>
+            </div>
+
         </div>
 
     </div>
