3db6007e61d4087516921371f9dc3bf707201ed1
Sathiskumar-S <sathiskumarsaravanan243214@gmail.com>
2024-01-08 16:57:01 +0530

    Admin page add new element feature implemented

--- admin/_templates/footer.php
+++ admin/_templates/footer.php
@@ -1,6 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 <script src="./js/activetoggle.js"></script>
-<script src="./js/formHandler.js"></script>
+<script src="./js/formHandler.js?<?php echo time();?> "></script>
 <script src="./js/script.js?<?php echo time(); ?>"></script>
 </body>
 </html>
