2d6681ed868395d2c6f3759359791b4c8434ddf2
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-01-23 12:22:34 +0530

    placement officer update

--- _templates/placement_officer.php
+++ _templates/placement_officer.php
@@ -1,19 +1,19 @@
 <div id="card" class="col-md-4">
-    <div class="card">
+    <div class="Tag">
         <div class="image"><img
                 src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1"></div>
-        <p class="title1">dhasarathan</p>
-        <p class="title1">dhasarathan</p>
-        <p class="title1"></p>
+        <p class="Banner">dhasarathan</p>
+        <p class="Banner">dhasarathan</p>
+        <p class="Banner"></p>
     </div>
-</div>
+
 
 <div id="card" class="col-md-4">
-    <div class="card">
+    <div class="Tag">
         <div class="image"><img
                 src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1"></div>
-        <p class="title1">dhasarathan</p>
-        <p class="title1">dhasarathan</p>
-        <p class="title1"></p>
+        <p class="Banner">dhasarathan</p>
+        <p class="Banner">dhasarathan</p>
+        <p class="Banner"></p>
     </div>
 </div>
