e5b958f5bf476c0153aca791e9db16a49136f55b
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-01-23 12:23:49 +0530

    hostel_adminstration_update

--- _templates/hostel_adminstration.php
+++ _templates/hostel_adminstration.php
@@ -1,5 +1,6 @@
-<div id="about_card" class="col-md-4">
-    <div class="card">
+<div id="about_sec" class="row">
+<div id="about_card_1" class="col-md-4">
+    <div class="card_row_1">
         <div class="image"><img
                 src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1">
         </div>
@@ -9,8 +10,8 @@
     </div>
 </div>
 
-<div id="about_card" class="col-md-4">
-    <div class="card">
+<div id="about_card_1" class="col-md-4">
+    <div class="card_row_2">
         <div class="image"><img
                 src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1">
         </div>
@@ -22,27 +23,32 @@
 
 </div>
 </div>
+<!-- second row -->
 
 <div id="about_sec" class="row">
-    <div class="col-md-3">
+    <div class="col-md-2"></div>
 
-    </div>
-    <div id="about_card" class="col-md-4">
-        <div class="card">
-            <div class="image"><img
-                    src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1"></div>
-            <p class="title1">dhasarathan</p>
-            <p class="title1">dhasarathan</p>
-            <p class="title1"></p>
+<div id="about_card_1" class="col-md-4">
+    <div class="card_row_3">
+        <div class="image"><img
+                src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1">
         </div>
+        <p class="title1">dhasarathan</p>
+        <p class="title1">dhasarathan</p>
+        <p class="title1"></p>
     </div>
+</div>
 
-    <div id="second" class="col-md-4">
-        <div class="card">
-            <div class="image"><img
-                    src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1"></div>
-            <p class="title1">dhasarathan</p>
-            <p class="title1">dhasarathan</p>
-            <p class="title1"></p>
+<div id="about_card_1" class="col-md-4">
+    <div class="card_row_4">
+        <div class="image"><img
+                src="https://th.bing.com/th/id/OIP.2-Hj54OLSN42NAS0giYAAgHaEo?pid=ImgDet&w=474&h=296&rs=1">
         </div>
+        <p class="title1">dhasarathan</p>
+        <p class="title1">dhasarathan</p>
+        <p class="title1"></p>
     </div>
+</div>
+
+</div>
+</div>
